97379f825541687a5dfc9644810d333bd33d097f
Ubuntu <ubuntu@ip-172-30-2-17.ec2.internal>
2020-04-27 16:12:49 +0000

    first commit

---
+++ aliases.sh
@@ -0,0 +1,4 @@
+echo 'alias sa="source activate"' >> ~/.bashrc
+echo 'alias sd="conda deactivate"' >> ~/.bashrc
+echo 'alias sb="source ~/.bashrc"' >> ~/.bashrc
+echo 'alias pid_airflow="lsof -i tcp:"' >> ~/.bashrc
