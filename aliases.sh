a5b708b8045d2af8639595e1c5fed89dbcc9e2db
Ubuntu <ubuntu@ip-172-30-2-17.ec2.internal>
2020-04-27 16:23:31 +0000

    updated scripts

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,4 @@
 echo 'alias sa="source activate"' >> ~/.bashrc
 echo 'alias sd="conda deactivate"' >> ~/.bashrc
 echo 'alias sb="source ~/.bashrc"' >> ~/.bashrc
-echo 'alias pid_airflow="lsof -i tcp:"' >> ~/.bashrc
+echo 'alias pid_airflow="lsof -i"' >> ~/.bashrc
