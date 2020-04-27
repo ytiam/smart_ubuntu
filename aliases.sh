77104e4e568a1ea63e923f6d337fb20b4efda0d7
Ubuntu <ubuntu@ip-172-30-2-17.ec2.internal>
2020-04-27 16:35:22 +0000

    updated scripts

--- aliases.sh
+++ aliases.sh
@@ -2,3 +2,7 @@ echo 'alias sa="source activate"' >> ~/.bashrc
 echo 'alias sd="conda deactivate"' >> ~/.bashrc
 echo 'alias sb="source ~/.bashrc"' >> ~/.bashrc
 echo 'alias pid_airflow="lsof -i"' >> ~/.bashrc
+echo 'alias pi="pip install"' >>  ~/.bashrc
+echo 'alias p2="python2"' >> ~/.bashrc
+echo 'alias p3="python3"' >> ~/.bashrc
+echo 'alias jn="jupyter notebook"' >> ~/.bashrc
