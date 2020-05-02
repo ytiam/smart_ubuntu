de869dbcf6779ab9efec341e52952044bec04a6c
atanu1ytiam <atanu.maity@ayata.com>
2020-05-02 14:53:08 +0530

    updated aliases

--- aliases.sh
+++ aliases.sh
@@ -5,4 +5,4 @@ echo 'alias pid_airflow="lsof -i"' >> ~/.bashrc
 echo 'alias pi="pip install"' >>  ~/.bashrc
 echo 'alias p2="python2"' >> ~/.bashrc
 echo 'alias p3="python3"' >> ~/.bashrc
-echo 'alias jn="jupyter notebook"' >> ~/.bashrc
+echo 'alias jn="jupyter notebook --no-browser"' >> ~/.bashrc
