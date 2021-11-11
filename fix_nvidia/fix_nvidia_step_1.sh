1fa2f2a3485aeb0c990e61f7589fd6a889e09055
ytiam <amrkmrc@gmail.com>
2021-11-11 21:16:33 +0530

    fix nvidia on ubuntu script added

---
+++ fix_nvidia/fix_nvidia_step_1.sh
@@ -0,0 +1,7 @@
+sudo apt purge nvidia-*
+sudo ppa-purge ppa:graphics-drivers/ppa
+sudo apt autoremove
+sudo apt auto-clean
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo ubuntu-drivers autoinstall
+sudo reboot
